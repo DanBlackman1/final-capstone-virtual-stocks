5162b47fa5309ac2827e77c30e5dd8efec4d6a03
daniel blackman <danielblackmantech@gmail.com>
2022-02-02 12:42:49 -0500

    added model viewgamesresponse and methods in accountdao to get an account and to retrieve a list of accounts when given a list of game objects, modified account model to take a gameid so it can be linked to a specific game more easily when returned to the front end of the application, modified app controller to return the viewgamesresponse object that contains a list of account objects and a list of game objects

--- java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
@@ -1,11 +1,15 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Account;
+import com.techelevator.model.Game;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class JdbcAccountDao implements AccountDao{
 
@@ -21,6 +25,7 @@ public class JdbcAccountDao implements AccountDao{
         account.setUserBalance(rs.getBigDecimal("user_balance"));
         account.setStockValue(rs.getBigDecimal("stock_value"));
         account.setDollarAmount(rs.getBigDecimal("dollar_amount"));
+        account.setGameId(rs.getInt("game_id"));
 
         return account;
     }
@@ -33,4 +38,27 @@ public class JdbcAccountDao implements AccountDao{
 
             return id;
     }
+
+    @Override
+    public Account getAccount(int userId, int gameId) {
+        Account account = null;
+        String sql = "SELECT a.account_id, a.user_balance, a.stock_value, a.dollar_amount, " +
+                "gd.game_id AS game_id FROM game_data gd JOIN account a ON a.account_id = gd.account_id WHERE " +
+                "gd.game_id = ? AND gd.user_id = ?";
+        SqlRowSet results = template.queryForRowSet(sql, gameId, userId);
+        account = mapRowToAccount(results);
+        return account;
+    }
+
+    @Override
+    public List<Account> listAccounts(List<Game> games, int userId) {
+        List<Account> accountList = new ArrayList<>();
+        for(Game userGame : games) {
+            Account account = getAccount(userId, userGame.getGameId());
+            accountList.add(account);
+        }
+        return accountList;
+    }
+
+
 }
