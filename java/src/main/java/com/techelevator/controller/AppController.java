26d02984b55c8ec654662a824ea2106df77ff1b7
daniel blackman <danielblackmantech@gmail.com>
2022-02-02 11:26:22 -0500

    added basic controller to server side app to return list of games, may need to add accounts to returned items - checking in with group first - also now able to return the id of a game that has been created

--- java/src/main/java/com/techelevator/controller/AppController.java
+++ java/src/main/java/com/techelevator/controller/AppController.java
@@ -1,19 +1,32 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.AccountDao;
 import com.techelevator.dao.GameDao;
+import com.techelevator.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @PreAuthorize("isAuthenticated()")
-//@RequestMapping ("/")
+@RequestMapping ("/myGames")
 public class AppController {
 
     @Autowired
-    GameDao dao;
+    GameDao gameDao;
+    @Autowired
+    AccountDao accountDao;
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public List<Game> viewGames(@PathVariable("id") int userId) {
+        return gameDao.getGames(userId);
+    }
+
+    @RequestMapping(path = "/{id}/createGame", method = RequestMethod.POST)
+    public int createGame(@RequestBody Game newGame) {
+        return gameDao.saveGame(newGame);
+    }
 
 }
